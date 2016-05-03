32799ea6061c07fce2010e7b474adad50ee2c2be
Matt <Matt@ADVP-MSCOTT>
2016-05-03 13:08:26 -0300

    new sidebar

---
+++ AmherstGolfClub/Controllers/RatesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmherstGolfClub.Controllers
+{
+    public class RatesController : Controller
+    {
+        // GET: Rates
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
