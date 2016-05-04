f369c0048e0223a4944663740341a6a1ef24b049
Greg <Greg@AVDP-GLAURETTE>
2016-05-04 13:50:23 -0300

    created tourny and golf context

---
+++ AmherstGolfClub/Controllers/TournamentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmherstGolfClub.Controllers
+{
+    public class TournamentController : Controller
+    {
+        // GET: Tournament
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tourny = db.Tournament.Find(id);
+            if (tourny == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tourny);
+        }
+
+    }
+}
---
+++ AmherstGolfClub/DAL/GolfContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.DAL
+{
+    public class GolfContext : DbContext
+    {       
+            public GolfContext() : base("DefaultConnection")
+            {
+            }
+
+            public DbSet<Tournament> Tournaments { get; set; }
+            public DbSet<Member> Members { get; set; }
+            public DbSet<Instructor> Instructors { get; set; }
+            public DbSet<Enrollment> Enrollments { get; set; }
+            public DbSet<Course> Courses { get; set; }
+            public DbSet<OfficeAssinment> OfficeAssignments { get; set; }
+            public DbSet<Department> Departments { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                //base.OnModelCreating(modelBuilder);
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+                /* Using Fluent API
+                 *  Take care of many to many relationships between instructor and course entities
+                 *  EF Code Firt can configure this for us, but if we do not override the names
+                 *  we will get mappings such as InstructorInstructorID for the InstructorID column
+                 */
+                modelBuilder.Entity<Course>().HasMany(c => c.Instructors).WithMany(i => i.Courses).Map(t => t.MapLeftKey("CourseID").MapRightKey("InstructorID").ToTable("CourseInstructor"));
+                //Above will create a junction (bridging) table called
+                //CourseInstructor
+                //with 2FK columns CourseID and InstructorID
+                //and a composite PK on CourseID + InstructorID
+            }
+
+        }
+    
+}
