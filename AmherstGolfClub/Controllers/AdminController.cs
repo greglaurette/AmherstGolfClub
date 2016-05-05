75cc501f397742add3ed396a1e13850e82e95a6d
Greg <Greg@AVDP-GLAURETTE>
2016-05-05 13:29:46 -0300

    created admin controller

---
+++ AmherstGolfClub/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmherstGolfClub.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
