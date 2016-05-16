9e0c39d3bd46124b6d7b0a1f764e7fa9f431df3f
Greg <Greg@AVDP-GLAURETTE>
2016-05-16 13:29:01 -0300

    asdfas

---
+++ AmherstGolfClub/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string Price { get; set; }
+        public int Quantity { get; set; }        
+        public string SubDepartment{ get; set; } 
+        public string ItemCategory { get; set; }
+        
+        
+    }
+}
