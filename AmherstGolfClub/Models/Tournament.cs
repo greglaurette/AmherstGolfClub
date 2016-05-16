3929a63d0e51af27f74ec55e99decce7b5394677
Matt <Matt@ADVP-MSCOTT>
2016-05-16 14:08:33 -0300

    made some models

---
+++ AmherstGolfClub/Models/Tournament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.Models
+{
+    public class Tournament
+    {
+        public int TournamentID { get; set; }
+
+        public string TournamentName { get; set; }
+
+        public DateTime TournamentDate { get; set; }
+
+    }
+}
---
+++ AmherstGolfClub/Models/TournamentPlayers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.Models
+{
+    public class TournamentPlayers
+    {
+        [Required]
+        public int PlayersID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(60)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(60)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Club { get; set; }
+
+        [Required]
+        public string Division { get; set; }
+
+        [Required]
+        public int DayOne { get; set; }
+
+        [Required]
+        public int DayTwo { get; set; }
+
+        public int Total {
+            get {
+                return DayOne + DayTwo; }
+        }
+
+    }
+}
