6fe2f849b5012b123be8e0963cbaddd900e96df2
Matt <Matt@ADVP-MSCOTT>
2016-05-16 13:41:42 -0300

    added dal folder

---
+++ AmherstGolfClub/DAL/GolfContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.DAL
+{
+    public class GolfContext:DbContext
+    {
+        public GolfContext() : base("DefaultConnection") { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
