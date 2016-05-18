f0bf2d852326897a7c65967b9c3175c8e0dcd958
Greg <Greg@AVDP-GLAURETTE>
2016-05-18 14:36:53 -0300

    COMPLETED TOURNY CONTROLLER AND VIEWS

--- AmherstGolfClub/Controllers/TournamentController.cs
+++ AmherstGolfClub/Controllers/TournamentController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace AmherstGolfClub.Controllers
 {
@@ -15,7 +16,8 @@ namespace AmherstGolfClub.Controllers
         // GET: Tournament
         public ActionResult Index()
         {
-            return View(db.Tournaments.ToList());
+            var tourny = db.Tournaments;
+            return View(tourny);
         }
 
         public ActionResult Details(int? id)
@@ -30,6 +32,80 @@ namespace AmherstGolfClub.Controllers
                 return HttpNotFound();
             }
             return View(tourny);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TournamentID,TournamentName,TournamentDate")] Tournament tourney)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tournaments.Add(tourney);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tourney);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tourney = db.Tournaments.Find(id);
+            if (tourney == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tourney);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tournament tourney = db.Tournaments.Find(id);
+            db.Tournaments.Remove(tourney);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tournament = db.Tournaments.Find(id);
+            if (tournament == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tournament);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TournamentID,TournamentName,TournamentDate")] Tournament tournament)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tournament).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tournament);
         }
 
     }
