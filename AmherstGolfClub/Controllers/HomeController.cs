e819c381e44298331b8227837f92bbfbbb3576dc
Greg <Greg@AVDP-GLAURETTE>
2016-05-19 12:06:13 -0300

    addded contact us form

--- AmherstGolfClub/Controllers/HomeController.cs
+++ AmherstGolfClub/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Text;
+using System.Web.UI;
+using AmherstGolfClub.Models;
 
 namespace AmherstGolfClub.Controllers
 {
@@ -22,6 +27,44 @@ namespace AmherstGolfClub.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactForm c)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.emailAddress.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.IsBodyHtml = false;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential("justforclass.oulton@gmail.com", "oulton!@#$");    
+                    msg.To.Add("greglaurette@gmail.com");
+                    msg.From = from;
+                    msg.Subject = c.emailSubject;                      
+                    sb.Append("Name: " + c.emailName);                                        
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Message: " + c.emailMessage);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+
+                }
+                catch(Exception)
+                {
+                    return View("Error");
+                }
+                
+            }
             return View();
         }
     }
---
+++ AmherstGolfClub/Models/ContactForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string emailName { get; set; }
+        [Required]
+        public string emailSubject { get; set; }
+        [Required]
+        public string emailMessage { get; set; }
+        [Required]
+        public string emailAddress { get; set; }
+    }
+}
