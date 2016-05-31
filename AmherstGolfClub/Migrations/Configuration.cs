db0552b759e0154c4c82840b4b52af6d54e40138
Greg <Greg@AVDP-GLAURETTE>
2016-05-31 19:05:39 -0300

    kkk;lkjk

--- AmherstGolfClub/Migrations/Configuration.cs
+++ AmherstGolfClub/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace AmherstGolfClub.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<AmherstGolfClub.DAL.GolfContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<AmherstGolfClub.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(AmherstGolfClub.DAL.GolfContext context)
+        protected override void Seed(AmherstGolfClub.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
